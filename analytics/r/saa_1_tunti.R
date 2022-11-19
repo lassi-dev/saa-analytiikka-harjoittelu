d42f233c41eca868d9257efa8018b5f2d9a51e61
Lassi Valtari <lassi.valtari@outlook.com>
2022-11-19 23:18:22 +0200

    Haetaan data SQL Serveriltä

--- analytics/r/saa_1_tunti.R
+++ analytics/r/saa_1_tunti.R
@@ -1,2 +1,15 @@
-library(ggplot2)
+#install.packages(ggplot2)
+#install.packages("odbc")
 
+library(ggplot2)
+library(RODBC)
+library(odbc)
+
+data_conn <- DBI::dbConnect(odbc::odbc(), "DATASOURCE", database = "SaaDB")
+
+# Harjoittelu mielessä vältetään SQL:ää
+
+saa_data <- dplyr::tbl(data_conn, "SAA_1H")
+
+# Listataan paikat
+saa_data %>% distinct(PAIKKA)
