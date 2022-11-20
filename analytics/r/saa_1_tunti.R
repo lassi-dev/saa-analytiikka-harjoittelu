decadb9c4765efe50f37beaed6596c8bec34a8d7
Lassi Valtari <lassi.valtari@outlook.com>
2022-11-20 14:59:42 +0200

    Rajataan hakukysely ja haetaan se koneen muistiin

--- analytics/r/saa_1_tunti.R
+++ analytics/r/saa_1_tunti.R
@@ -2,8 +2,8 @@
 #install.packages("odbc")
 
 library(ggplot2)
-library(RODBC)
 library(odbc)
+library(dplyr)
 
 data_conn <- DBI::dbConnect(odbc::odbc(), "DATASOURCE", database = "SaaDB")
 
@@ -13,3 +13,21 @@ saa_data <- dplyr::tbl(data_conn, "SAA_1H")
 
 # Listataan paikat
 saa_data %>% distinct(PAIKKA)
+
+paikka_rajaus <- "xx.xxxxx xx.xxxxx "
+aika_alku <- '2021-12-01 00:00:00.00'
+aika_loppu <- '2021-12-31 23:00:00.00'
+
+# Rajataan tiettyyn paikkaan ja aikaan
+saa_data <- dplyr::tbl(data_conn, "SAA_1H") %>% 
+                    filter(PAIKKA == paikka_rajaus
+                          & AIKA >= aika_alku
+                          & AIKA <= aika_loppu) %>%
+                    select(ID, AIKA, LAMPOTILA, LAMPOTILA_MAKSIMI, LAMPOTILA_MINIMI) %>%
+                    arrange(AIKA)
+
+show_query(saa_data)
+
+saa <- collect(saa_data)
+
+head(saa)
